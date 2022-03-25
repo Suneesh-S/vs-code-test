acde671b2e386ae579b74403da02def14faf0262
Suneesh-S <122005142@sastra.ac.in>
2022-03-25 15:14:05 +0530

    added 5 and 6th band

--- resistor.c
+++ resistor.c
@@ -2,11 +2,13 @@
 #include <math.h>
 
 void    print_codes( void );     /* menu of codes */
-double  decode_char( char* );
-double  tolerance_char( char* );
+double  decode_char( char  );
+double  tolerance_char( char  );
+double ppm_char( char  );
+
 int main()
 {
-     char*    code[10];     /* one code per band */
+     char    code[10];     /* one code per band */
      double  resistance;
      double  color[10];  /* decoded values */
      
@@ -17,37 +19,52 @@ int main()
      printf( "\n\n\tEnter no of colour Bands(4 or 5 or 6): " );
      scanf("%d",&N);
      getchar();
-     printf( "\n\n\tEnter the colour Bands:\n" );
+     printf( "\n\n\tEnter the colour Bands:" );
      /* Read three character codes. */
      for(int i=0;i<N;i++){
          code[i]=getchar();
      }
 
      /* Decode each character code. */
-     
-         for(int i=0;i<N-1;i++){
-     color[i] = decode_char(&code[i]);
+     if(N==6){
+         for(int i=0;i<N-2;i++){
+     color[i] = decode_char(code[i]);
     }
-     color[N-1]=tolerance_char(&code[N-1]);
-     
+     color[N-2]=tolerance_char(code[N-2]);
+     color[N-1]=ppm_char(code[N-1]);
+     }else{
+         for(int i=0;i<N-1;i++){
+     color[i] = decode_char(code[i]);
+    }
+     color[N-1]=tolerance_char(code[N-1]);
+     }
+
      /* Check whether codes were legal. */
      for(int i=0;i<N;i++){
          if(color[i]==-1){
            printf( "\n\n\tBad code -- cannot compute resistance\n" );
            flag=1;
            break;
-         }else if(i<N-2){
-             num=num*10+color[i];
+         }
+         if(N<6 && i<N-2){
+              num=num*10+color[i];
+         }else if(N==6 && i<N-3){
+              num=num*10+color[i];
+         }else if(N<6){
+             multiplier=pow(10,color[N-2]);
+             continue;
          }else{
-             multiplier=pow(10,color[i]);
+              multiplier=pow(10,color[N-3]);
+              continue;
          }
     }
       resistance=num*multiplier;
      if(flag==0){
-        
-           
+         if(N==6){
+            printf( "\n\n\tResistance in ohms:\t%.2f tolerance %.2f %% %.1f ppm \n", resistance,color[N-2],color[N-1] );
+         }else{
           printf( "\n\n\tResistance in ohms:\t%.2f tolerance %.1f %% \n", resistance,color[N-1] );  
-        
+         }
        
      }
    return 0;
@@ -74,13 +91,15 @@ void  print_codes( void )
    printf( "\tViolet------------------> V\n" );
    printf( "\tGrey--------------------> A\n" );
    printf( "\tWhite-------------------> W\n" );
+   printf( "\tGold--------------------> U\n" );
+   printf( "\tSilver------------------> Z\n" );
 }
 
 
 
-double decode_char( char* c )
+double decode_char( char c )
 {   
-     switch ( *c ) {
+     switch ( c ) {
      case 'B':
           return 0.0;
      case 'N':
@@ -108,9 +127,9 @@ double decode_char( char* c )
      
 }
 
-double tolerance_char( char* c )
+double tolerance_char( char c )
 {   
-     switch ( *c ) {
+     switch ( c ) {
      case 'N':
           return 1.0;
      case 'R':
@@ -129,6 +148,27 @@ double tolerance_char( char* c )
           return 5.0;
      case 'Z':
           return 10.0;
+     default:
+          return -1;     /* illegal code */
+     }
+}
+
+double ppm_char( char c )
+{   
+     switch ( c ) {
+     case 'N':
+          return 100;
+     case 'R':
+          return 50;
+     case 'O':
+          return 15;
+     case 'Y':
+          return 25;
+     case 'E':
+          return 10;
+     case 'V':
+          return 5;
+     
      default:
           return -1;     /* illegal code */
      }
